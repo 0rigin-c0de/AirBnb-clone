eb6f8dd8886d4403dc6d1260e16f508bdd747d14
0rigin-c0de <shunnu807@gmail.com>
2023-02-12 11:04:31 +0530

    initial-commit

---
+++ components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img className="nav--logo" src="/images/airbnb25.png"/>
+        </nav>
+    )
+    
+}
